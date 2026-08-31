b0c2df4c4818b42547ddee677393940e623a9698
ravi sharma <raviisharmaa.2004@gmail.com>
2026-08-31 21:12:22 +0530

    refactor minimumDeletions function for improved clarity and efficiency

--- leetcode/2091.Removing_Minimum_and_Maximum_From_Array.cpp
+++ leetcode/2091.Removing_Minimum_and_Maximum_From_Array.cpp
@@ -22,3 +22,67 @@
 //         return min(find,removebothside);
 //     }
 // };
+
+// class Solution {
+// public:
+//     int minimumDeletions(vector<int>& nums) {
+//         int MinNum = 0,MaxNum = 0;
+//         for(int i = 0; i < nums.size(); i++)
+//         {
+//             if(nums[i] > nums[MaxNum])
+//             {
+//                 MaxNum = i;
+//             }
+//             else if(nums[i] < nums[MinNum])
+//             {
+//                 MinNum = i;
+//             }
+//         }
+//         int leftside = min(MaxNum,MinNum);
+//         int rightside = max(MaxNum,MinNum);
+//         int n = nums.size();
+//         return min({rightside + 1, n - leftside, (leftside + 1 + n - rightside)});
+//     }
+// };
+
+
+
+// class Solution {
+// public:
+//     int minimumDeletions(vector<int>& nums) {
+//         int n = nums.size();
+        
+//         // Find positions of min and max elements
+//         auto minIt = min_element(nums.begin(), nums.end());
+//         auto maxIt = max_element(nums.begin(), nums.end());
+        
+//         int minIdx = minIt - nums.begin();
+//         int maxIdx = maxIt - nums.begin();
+        
+//         int left = min(minIdx, maxIdx);
+//         int right = max(minIdx, maxIdx);
+        
+//         // Three strategies
+//         int deleteFromLeft = right + 1;
+//         int deleteFromRight = n - left;
+//         int deleteFromBoth = (left + 1) + (n - right);
+        
+//         return min({deleteFromLeft, deleteFromRight, deleteFromBoth});
+//     }
+// };
+
+
+
+// class Solution {
+// public:
+//     int minimumDeletions(vector<int>& nums) {
+//         int n = nums.size();
+//         int minIdx = min_element(nums.begin(), nums.end()) - nums.begin();
+//         int maxIdx = max_element(nums.begin(), nums.end()) - nums.begin();
+        
+//         int a = min(minIdx, maxIdx);
+//         int b = max(minIdx, maxIdx);
+        
+//         return min({b + 1, n - a, a + 1 + n - b});
+//     }
+// };
