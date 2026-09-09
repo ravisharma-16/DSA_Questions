ffbe34590a21ac4b5e2e898da56ee2a201d1917b
ravi sharma <raviisharmaa.2004@gmail.com>
2026-09-09 20:24:06 +0530

    add initial implementation of hasCycle function for detecting cycles in a linked list

---
+++ leetcode/141.Linked_List_Cycle.cpp
@@ -0,0 +1,48 @@
+// /**
+//  * Definition for singly-linked list.
+//  * struct ListNode {
+//  *     int val;
+//  *     ListNode *next;
+//  *     ListNode(int x) : val(x), next(NULL) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         unordered_set<ListNode*>mp;
+//         ListNode *temp = head;
+//         while(temp)
+//         {
+//             if(mp.count(temp))
+//             {
+//                 return true;
+//             }
+//             mp.insert(temp);
+//             temp = temp->next;
+//         }
+//         return false;
+//     }
+// };
+
+
+
+
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+//         if(!head || !head->next) return false;
+        
+//         ListNode* slow = head;
+//         ListNode* fast = head;
+        
+//         while(fast && fast->next) {
+//             slow = slow->next;        // Move one step
+//             fast = fast->next->next;  // Move two steps
+            
+//             if(slow == fast) {
+//                 return true;  // Cycle detected
+//             }
+//         }
+//         return false;  // No cycle
+//     }
+// };
